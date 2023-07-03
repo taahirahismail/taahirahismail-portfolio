f96ce6fc2d4531b12f108e450bc1b2ab35d3f54f
taahirahismail <taahirah.i@gmail.com>
2023-07-03 12:47:49 +0200

    got most of it working

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ export default createStore({
     experience: null,
     projects: null,
     project: null,
-    testimonials: null
+    testimonials: null,
+    playlist: null
   },
   mutations: {
     setExperience: (state, experience) => {
@@ -19,28 +20,36 @@ export default createStore({
     },
     setTestimonials: (state, testimonials) => {
       state.testimonials = testimonials;
+    },
+    setPlaylist: (state, playlist) => {
+      state.playlist = playlist;
     }
   },
   actions: {
     getExperience: async (context) => {
-      fetch("https//:taahirahismail.github.io/vue-eomp-data/edu-exp-db.json")
+      fetch("https://taahirahismail.github.io/vue-eomp-data/edu-exp-db.json")
       .then((res) => res.json())
       .then((experience) => context.commit("setExperience", experience))
     },
     getProjects: async (context) => {
-      fetch("https//:taahirahismail.github.io/vue-eomp-data/projects-db.json")
+      fetch("https://taahirahismail.github.io/vue-eomp-data/projects-db.json")
       .then((res) => res.json())
       .then((projects) => context.commit("setProjects", projects))
     },
     getProject: async (context, id) =>{
-      fetch("https//:taahirahismail.github.io/vue-eomp-data/projects-db.json/" + id)
+      fetch("https://taahirahismail.github.io/vue-eomp-data/projects-db.json/" + id)
       .then((res) => res.json())
       .then((project) => context.commit("setProject", project))
     },
     getTestimonials: async (context) => {
-      fetch("https//:taahirahismail.github.io/vue-eomp-data/testimonials-db.json")
+      fetch("https://taahirahismail.github.io/vue-eomp-data/testimonials-db.json")
       .then((res) => res.json())
       .then((testimonials) => context.commit("setTestimonials", testimonials))
+    },
+    getPlaylist: async (context) => {
+      fetch("https://taahirahismail.github.io/vue-eomp-data/passion-board-db.json")
+      .then((res) => res.json())
+      .then((playlist) => context.commit("setPlaylist", playlist))
     }
   }
 })
